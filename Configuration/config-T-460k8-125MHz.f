01a9c63a90fd2e3b111b8b390eb9826f5858977f
Willem_O <w.ouwerkerk@kader.hcc.nl>
2023-11-14 10:45:58 +0100

    Update config-T-460k8-125MHz.f
    typo

--- Configuration/config-T-460k8-125MHz.f
+++ Configuration/config-T-460k8-125MHz.f
@@ -1,4 +1,4 @@
-(* Changing the configuration of noForth T duo (Current use~29.5mA)
+(* Changing the configuration of noForth t duo (Current use~29.5mA)
 
     0 CFG  = Clock frequency in Hz
     1 CFG  = Used UART ( 0 or 1 ) only 0 is valid for now
