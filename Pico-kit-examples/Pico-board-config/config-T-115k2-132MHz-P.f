842dc6b275d608b4f325ba9504de350d74db6cd2
Willem_O <w.ouwerkerk@kader.hcc.nl>
2025-12-20 22:10:43 +0100

    Update config-T-115k2-132MHz-P.f
    Corrected all comments

--- Pico-kit-examples/Pico-board-config/config-T-115k2-132MHz-P.f
+++ Pico-kit-examples/Pico-board-config/config-T-115k2-132MHz-P.f
@@ -1,25 +1,24 @@
-(* Changing the configuration of noForth t (duo) (Current use~29.5mA)
+(* Changing the configuration of noForth t (solo and duo) (Current use~29.5mA)
 
-    0 CFG  = Clock frequency in Hz
-    1 CFG  = Used UART ( 0 or 1 ) only 0 is valid for now
+    0 CFG  = Used GPIO pin for S? & clock frequency in Hz
+    1 CFG  = Used UART ( 40034000 or 40038000 )
     2 CFG  = Baudrate in bits per second
-    3 CFG  = Used GPIO pin for S?
+    3 CFG  = Used GPIO input address register
     4 CFG  = Boot method
-
-    GROW   = Resize noForth t with the number of bytes from the stack
 
 Valid data for these parameters are:
     Clock    = 12, 30, 60, 120, 125, 132, 200 & 250 MHz
-    Uart     = 0  (will be upgraded when this version is stable)
+    Uart     = UART0 = 40034000, UART1 = 40038000
     Baudrate = Any baudrate like 9600, 115200 until 921600 was tested ok
-    S? pin   = GPIO 24, but any free GPIO pin will do
-    Boot     = 1 = Single image noForth t
-               2 = noForth t duo
+    S? pin   = D0000004, but any address may be used
+    Boot     = 0 = noForth t solo
+               1 = noForth t duo
 
     FREEZE   = Save bootup images
     FREEZE2  = Save spare images
     COLD     = (Re)load bootup images
     COLD2    = (Re)load spare images
+    GROW     = Resize noForth t with the number of bytes from the stack
 
 *)
 
@@ -41,3 +40,4 @@ hex  config             \ Test new configuration
 \ freeze2       \ Save as spare system, boots when you type COLD2
 
 \ End
+
