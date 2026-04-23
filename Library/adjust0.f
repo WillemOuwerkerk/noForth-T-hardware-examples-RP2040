34f53bf564797a386fd6c0268064aec67075bccc
Willem_O <w.ouwerkerk@kader.hcc.nl>
2026-04-23 17:35:45 +0200

    Updated ADJUST0.f
    now sets the PIO & HARDWARE pointers too

--- Library/adjust0.f
+++ Library/adjust0.f
@@ -5,4 +5,9 @@ v: inside
     10081000 dup to lib-org  begin  c@+ FF = until  1- to libhere ;
 
 adjust cr .( Library pointers are set: ) lib-org . libhere .
+
+s" CORE\"       find-chapter 1- to hardware)
+s" BIT-TOGGLE1" find-chapter 1- to pio)
+
+cr .( Shortcuts HARDWARE & PIO are also set: ) hardware) . pio) .
 v: forth
